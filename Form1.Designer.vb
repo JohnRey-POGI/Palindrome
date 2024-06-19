137281ae2d966ef6b9c057fd621895fee943289c
JohnReyPOGI <johnreystaana@gmail.com>
2024-06-19 14:38:26 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnchecker = New System.Windows.Forms.Button()
+        Me.txtnumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnchecker
+        '
+        Me.btnchecker.Location = New System.Drawing.Point(119, 132)
+        Me.btnchecker.Name = "btnchecker"
+        Me.btnchecker.Size = New System.Drawing.Size(100, 34)
+        Me.btnchecker.TabIndex = 0
+        Me.btnchecker.Text = "Check"
+        Me.btnchecker.UseVisualStyleBackColor = True
+        '
+        'txtnumber
+        '
+        Me.txtnumber.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtnumber.Location = New System.Drawing.Point(77, 68)
+        Me.txtnumber.Name = "txtnumber"
+        Me.txtnumber.Size = New System.Drawing.Size(182, 43)
+        Me.txtnumber.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(50, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(249, 37)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "INPUT A NUMBER"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 178)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtnumber)
+        Me.Controls.Add(Me.btnchecker)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnchecker As Button
+    Friend WithEvents txtnumber As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnchecker.Click
+        Dim r As Integer
+        Dim num As Integer
+        Dim temp As Integer
+
+        If IsNumeric(txtnumber.Text) Then
+            num = CInt(txtnumber.Text)
+        Else
+            MessageBox.Show("Please enter a valid number")
+            Exit Sub
+        End If
+
+        If txtnumber.Text.Length < 3 Then
+            MessageBox.Show("Please enter a value not less than 3 characters")
+            Exit Sub
+        End If
+
+        Dim sum As Integer = 0
+        temp = num
+        While num > 0
+            r = num Mod 10
+            sum = (sum * 10) + r
+            num /= 10
+        End While
+
+        If temp = sum Then
+            MessageBox.Show(temp & " is a palindrome number")
+        Else
+            MessageBox.Show(temp & " is NOT a palindrome number")
+        End If
+    End Sub
+End Class
